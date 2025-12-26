1d3f58fc6383a552f98ebfdaed7ed7746559baa9
MARTD2004 <hackmeifyoucanteam@gmail.com>
2025-12-26 18:59:35 +0300

    dd ystem.et.ttp using for ttplientandler

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
